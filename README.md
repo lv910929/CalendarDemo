14bc1dacef3ee13871b4281e1eb6a4757dad21a1
lv910929 <lv910929@live.com>
2016-10-27 11:11:57 +0800

    change ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # CalendarDemo
+> 自定义的日历控件
+
+#### 简单使用
+样式设置：
+* colorSelectBG：选中日期的背景色
+* colorSelectDay：选中日期的颜色
+* colorToday：今天日期颜色
+* colorMonthView：日历的整个背景色
+* colorWeekday：工作日的颜色
+* colorWeekend：周末的颜色
+* colorWork：班颜色
+* colorDesc：描述文字颜色
+* sizeDay：日期大小
+* sizeDesc：描述文字大小
+* dateHeight：日期高度
+* colorLine：线条颜色
+* smoothMode：滑动模式，0为有滑动模式，1没有滑动效果。
+使用自定义样式就需要分别继承实现日期和周的样式，然后通过方法进行设置。设置日期日历界面的样式 calendarView.setDayTheme(new IDayTheme());
+
+其他功能：
+1、 支持点击事件
+``` 
+calendarView.setDateClick(new MonthView.IDateClick() {
+    @Override
+    public void onClickOnDate(int year, int month, int day) {
+        String text = "点击了:" + year + "-" + month + "-" + day;
+        Snackbar.make(activityMain, text, Snackbar.LENGTH_SHORT).show();
+    }
+});
+```
+
+2、 支持事务的添加
+```
+list.add(new CalendarInfo(currYear, currMonth, 3, "居家家居团购", CalendarInfo.COLOR_ORANGE));
+list.add(new CalendarInfo(currYear, currMonth, 11, "居家家居团购", CalendarInfo.COLOR_GREEN));
+list.add(new CalendarInfo(currYear, currMonth, 19, "居家家居团购", CalendarInfo.COLOR_YELLOW));
+list.add(new CalendarInfo(currYear, currMonth, 24, "居家家居团购", CalendarInfo.COLOR_BLUE));
+list.add(new CalendarInfo(currYear, currMonth, 29, "居家家居团购", Color.parseColor("#DD5044")));
+calendarView.setCalendarInfos(list);
+```
+
+CalendarInfo类字段示意：
+* year 事务年份
+* month 事务月份
+* day 事务日期号
+* des 事务描述
+* rest 活动日期背景色
+
+#### 截图
+![截图](https://github.com/lv910929/CalendarDemo/blob/master/art/screenshot_1.png)
+
--- app/src/main/java/com/lv/calendardemo/MainActivity.java
+++ app/src/main/java/com/lv/calendardemo/MainActivity.java
@@ -1,10 +1,14 @@
 package com.lv.calendardemo;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 
 import com.lv.calendardemo.entity.CalendarInfo;
 import com.lv.calendardemo.view.GridCalendarView;
+import com.lv.calendardemo.view.MonthView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -12,6 +16,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout activityMain;
     private GridCalendarView calendarView;
 
     private List<CalendarInfo> list;
@@ -33,11 +38,19 @@ public class MainActivity extends AppCompatActivity {
         list.add(new CalendarInfo(currYear, currMonth, 11, "居家家居团购", CalendarInfo.COLOR_GREEN));
         list.add(new CalendarInfo(currYear, currMonth, 19, "居家家居团购", CalendarInfo.COLOR_YELLOW));
         list.add(new CalendarInfo(currYear, currMonth, 24, "居家家居团购", CalendarInfo.COLOR_BLUE));
-        list.add(new CalendarInfo(currYear, currMonth, 29, "居家家居团购", CalendarInfo.COLOR_PURPLE));
+        list.add(new CalendarInfo(currYear, currMonth, 29, "居家家居团购", Color.parseColor("#DD5044")));
     }
 
     private void initUI() {
+        activityMain = (RelativeLayout) findViewById(R.id.activity_main);
         calendarView = (GridCalendarView) findViewById(R.id.calendar_view);
         calendarView.setCalendarInfos(list);
+        calendarView.setDateClick(new MonthView.IDateClick() {
+            @Override
+            public void onClickOnDate(int year, int month, int day) {
+                String text = "点击了:" + year + "-" + month + "-" + day;
+                Snackbar.make(activityMain, text, Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 }
